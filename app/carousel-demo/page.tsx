5dd67d4bb1f064e2c0b2922089104c5e910e4b7b
Michael <88445485+mjnyland@users.noreply.github.com>
2025-09-24 09:11:27 -0400

    removed unused folders

--- app/carousel-demo/page.tsx
+++
@@ -1,9 +0,0 @@
-import SimpleScrollCarousel from "@/components/SimpleScrollCarousel";
-
-export default function CarouselDemoPage() {
-  return (
-    <main>
-      <SimpleScrollCarousel />
-    </main>
-  );
-}
